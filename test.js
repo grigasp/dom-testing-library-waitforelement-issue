30be08e3a6d64fa2359bd6971f0e98d9b4633933
grigas.petraitis <grigas.petraitis@bentley.com>
2018-09-14 16:11:33 +0300

    Add debug script

--- test.js
+++ test.js
@@ -7,8 +7,9 @@ it("shouldn't hang after succeeding", async () => {
     const myCallback = () => {
         return didMakeMutation;
     };
-    const result = waitForElement(myCallback, { container });
+    const result = waitForElement(myCallback, { container, timeout: 9999999 });
     container.appendChild(createElement());
     didMakeMutation = true;
     await new Promise((resolve) => setTimeout(resolve, 50));
+    await result;
 });
